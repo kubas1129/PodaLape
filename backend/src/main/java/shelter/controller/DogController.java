910fb3e77fd745790d811492fe70ad1405fe8a4a
lukasz <wolskiaxiom@interia,pl>
2019-04-07 02:15:06 +0200

    add post and put for dog

--- backend/src/main/java/shelter/controller/DogController.java
+++ backend/src/main/java/shelter/controller/DogController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import shelter.model.Dog;
 import shelter.model.DogRepository;
-import shelter.model.Shelter;
 
 import java.util.List;
 import java.util.Map;
